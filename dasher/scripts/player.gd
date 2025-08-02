3f729f3dfc17beba90119e0bbfcf7703c98bae92
UnstoppableBlob <96858668+UnstoppableBlob@users.noreply.github.com>
2025-08-01 18:34:27 -0700

    Added basic attacking animations.

--- dasher/scripts/player.gd
+++ dasher/scripts/player.gd
@@ -1,23 +1,45 @@
 extends CharacterBody2D
 
 @onready var anim = $AnimatedSprite2D
+@onready var anim_player = $AnimationPlayer
 
 const SPEED = 50
-
+@export var is_attacking = false
 
 func _physics_process(delta: float) -> void:
-	var input_dir = Input.get_vector("left", "right", "up", "down")
+	var input_dir = Vector2.ZERO
+	input_dir = Input.get_vector("left", "right", "up", "down")
 	input_dir = input_dir.normalized()
 	if input_dir.x != 0:
-		anim.play("walkright")
+		if Input.is_action_just_pressed("attack"):
+			anim_player.play("isattacking")
+			anim.play("attackright")
+			print("ran")
+		if !is_attacking:
+			anim.play("walkright")
 		anim.flip_h = input_dir.x < 0
 	elif input_dir.y == 1:
-		anim.play("walkfront")
+		if Input.is_action_just_pressed("attack"):
+			anim_player.play("isattacking")
+			anim.play("attackfront")
+		if !is_attacking:
+			anim.play("walkfront")
 	elif input_dir.y == -1:
-		anim.play("walkback")
+		if Input.is_action_just_pressed("attack"):
+			anim_player.play("isattacking")
+			anim.play("attackback")
+		if !is_attacking:
+			anim.play("walkback")
 	else:
-		anim.play("idlefront")
+		if Input.is_action_just_pressed("attack"):
+			anim_player.play("isattacking")
+			anim.play("attackfront")
+		if !is_attacking:
+			anim.play("idlefront")
+	
+
 		
-	velocity = input_dir * SPEED
+	if !is_attacking:
+		velocity = input_dir * SPEED
 
 	move_and_slide()
