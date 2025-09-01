b51cb36656a623f9b1e5489d71ba2781ae1d8b56
UnstoppableBlob <96858668+UnstoppableBlob@users.noreply.github.com>
2025-09-01 11:12:51 -0700

    made the 1 better

--- dasher/scripts/1.gd
+++ dasher/scripts/1.gd
@@ -4,11 +4,10 @@ extends RigidBody2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
-	#global_position = Vector2.ZERO
+	global_position = Vector2.ZERO
 	global_rotation = randf_range(0, TAU)
 	var forward_direction = Vector2.RIGHT.rotated(global_rotation)
-	linear_velocity = forward_direction * 250
-
+	apply_central_impulse(forward_direction * 2000)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	print(";alkjdfsssssssssssssssssssssssssssssssss")
