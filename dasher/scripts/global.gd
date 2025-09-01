6c25496bb1b9e08c537721a5c5e0afa72944890f
UnstoppableBlob <96858668+UnstoppableBlob@users.noreply.github.com>
2025-09-01 10:16:40 -0700

    Added assets.
    Added assets from www.kenney.nl and related assets to the project.
    
    www.kenney.nl

--- dasher/scripts/global.gd
+++ dasher/scripts/global.gd
@@ -2,3 +2,10 @@ extends Node2D
 
 
 @onready var tilemap = $levelcont/TileMap
+@onready var player = $Player
+
+
+func _on_spawn_timeout() -> void:
+	print("spawned")
+	player.spawned = true
+	player.visible = true
--- dasher/scripts/player.gd
+++ dasher/scripts/player.gd
@@ -11,55 +11,62 @@ var dashing = false
 var dash_speed = 200
 var can_dash = true
 
+var spawned = false
+
 func _ready() -> void:
+	visible = false
+	spawned = false
 	dash.visible = false
+	Global.player = self
 
 func _physics_process(delta: float) -> void:
-	var input_dir = Vector2.ZERO
-	input_dir = Input.get_vector("left", "right", "up", "down")
-	input_dir = input_dir.normalized()
-	if input_dir.x != 0:
-		if Input.is_action_just_pressed("attack"):
-			anim_player.play("isattacking")
-			anim.play("attackright")
-			print("ran")
-		if !is_attacking:
-			anim.play("walkright")
-		anim.flip_h = input_dir.x < 0
-	elif input_dir.y == 1:
-		if Input.is_action_just_pressed("attack"):
-			anim_player.play("isattacking")
-			anim.play("attackfront")
-		if !is_attacking:
-			anim.play("walkfront")
-	elif input_dir.y == -1:
-		if Input.is_action_just_pressed("attack"):
-			anim_player.play("isattacking")
-			anim.play("attackback")
-		if !is_attacking:
-			anim.play("walkback")
-	else:
-		if Input.is_action_just_pressed("attack"):
-			anim_player.play("isattacking")
-			anim.play("attackfront")
-		if !is_attacking:
-			anim.play("idlefront")
+	print(spawned)
+	if spawned:
+		var input_dir = Vector2.ZERO
+		input_dir = Input.get_vector("left", "right", "up", "down")
+		input_dir = input_dir.normalized()
+		if input_dir.x != 0:
+			if Input.is_action_just_pressed("attack"):
+				anim_player.play("isattacking")
+				anim.play("attackright")
+				print("ran")
+			if !is_attacking:
+				anim.play("walkright")
+			anim.flip_h = input_dir.x < 0
+		elif input_dir.y == 1:
+			if Input.is_action_just_pressed("attack"):
+				anim_player.play("isattacking")
+				anim.play("attackfront")
+			if !is_attacking:
+				anim.play("walkfront")
+		elif input_dir.y == -1:
+			if Input.is_action_just_pressed("attack"):
+				anim_player.play("isattacking")
+				anim.play("attackback")
+			if !is_attacking:
+				anim.play("walkback")
+		else:
+			if Input.is_action_just_pressed("attack"):
+				anim_player.play("isattacking")
+				anim.play("attackfront")
+			if !is_attacking:
+				anim.play("idlefront")
 
-	if Input.is_action_just_pressed("dash") and can_dash:
-		dash.emitting = true
-		dash.visible = true
-		dashing = true
-		can_dash = false
-		$Timer2.start()
-		$Timer.start()
+		if Input.is_action_just_pressed("dash") and can_dash:
+			dash.emitting = true
+			dash.visible = true
+			dashing = true
+			can_dash = false
+			$Timer2.start()
+			$Timer.start()
 
-	if !dashing:
-		if !is_attacking:
-			velocity = input_dir * SPEED
-	else:
-		velocity = input_dir * dash_speed
-	
-	move_and_slide()
+		if !dashing:
+			if !is_attacking:
+				velocity = input_dir * SPEED
+		else:
+			velocity = input_dir * dash_speed
+		
+		move_and_slide()
 
 
 func _on_timer_timeout() -> void:
