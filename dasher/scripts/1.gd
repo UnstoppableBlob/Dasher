e87c93f8ba6feff87b7243f1372f815d4d48375c
UnstoppableBlob <96858668+UnstoppableBlob@users.noreply.github.com>
2025-09-01 11:00:03 -0700

    started getting the flying number to work.

---
+++ dasher/scripts/1.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	#global_position = Vector2.ZERO
+	global_rotation = randf_range(0, TAU)
+	var forward_direction = Vector2.RIGHT.rotated(global_rotation)
+	linear_velocity = forward_direction * 250
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print(";alkjdfsssssssssssssssssssssssssssssssss")
