a67014265c25b1c3e1cf5a41051388143402da34
UnstoppableBlob <96858668+UnstoppableBlob@users.noreply.github.com>
2025-09-01 11:23:31 -0700

    changed some sizing

--- dasher/scripts/1.gd
+++ dasher/scripts/1.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 	global_position = Vector2.ZERO
 	global_rotation = randf_range(0, TAU)
 	var forward_direction = Vector2.RIGHT.rotated(global_rotation)
-	apply_central_impulse(forward_direction * 2000)
+	apply_central_impulse(forward_direction * 250)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	print(";alkjdfsssssssssssssssssssssssssssssssss")
