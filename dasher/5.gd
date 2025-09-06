be98e6610b0f3b5f6ef0abe90fd7cae2c8e0eeff
UnstoppableBlob <96858668+UnstoppableBlob@users.noreply.github.com>
2025-09-05 18:55:01 -0700

    Changed the say that the numbers move, and made them move slower. Fixed some bugs, too.

--- dasher/5.gd
+++ dasher/5.gd
@@ -8,8 +8,18 @@ func _ready() -> void:
 	global_position = Vector2.ZERO
 	global_rotation = randf_range(0, TAU)
 	var forward_direction = Vector2.RIGHT.rotated(global_rotation)
-	apply_central_impulse(forward_direction * 200)
-
+	apply_central_impulse(forward_direction * 100)
+var x = 1
+var y = 1
+func _process(delta: float) -> void:
+	y += 1
+	if y == 150:
+		y = 0
+		linear_velocity = Vector2.ZERO
+		angular_velocity = 0.0
+		global_rotation = randf_range(0, TAU)
+		var forward_direction = Vector2.RIGHT.rotated(global_rotation)
+		apply_central_impulse(forward_direction * 100)
 func _on_area_2d_body_entered(body) -> void:
 	if other && body is CharacterBody2D:
 		Global.player_speed = 50
--- dasher/scripts/1.gd
+++ dasher/scripts/1.gd
@@ -2,13 +2,27 @@ extends RigidBody2D
 
 var other = false
 
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
 	global_position = Vector2.ZERO
 	global_rotation = randf_range(0, TAU)
 	var forward_direction = Vector2.RIGHT.rotated(global_rotation)
-	apply_central_impulse(forward_direction * 200)
+	apply_central_impulse(forward_direction * 100)
+var x = 1
+var y = 1
+func _process(delta: float) -> void:
+	y += 1
+	if y == 150:
+		y = 0
+		linear_velocity = Vector2.ZERO
+		angular_velocity = 0.0
+		global_rotation = randf_range(0, TAU)
+		var forward_direction = Vector2.RIGHT.rotated(global_rotation)
+		apply_central_impulse(forward_direction * 100)
+	
 
 func _on_area_2d_body_entered(body) -> void:
 	if other && body is CharacterBody2D:
